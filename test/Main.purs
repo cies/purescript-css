d43b393f70624d01402f69f03b93a24654865d2d
Cies Breijs <cies AT kde DOT nl>
2016-11-10 09:58:16 +0100

    Bower file slightly changed

--- test/Main.purs
+++ test/Main.purs
@@ -7,6 +7,15 @@ import Control.Monad.Eff.Exception (EXCEPTION, error, throwException)
 import CSS (Rendered, Path(..), Predicate(..), Refinement(..), Selector(..), renderedSheet, renderedInline, fromString, selector, block, display, render, borderBox, boxSizing, contentBox, blue, color, body, px, dashed, border, inlineBlock, red, (?))
 import Data.Maybe (Maybe(..))
 
+import Control.Monad.Eff.Console (log, CONSOLE)
+import Data.Generic
+import Data.Array (last)
+import Data.Maybe (fromMaybe)
+import Data.String (split, toLower, drop)
+import Data.String.Regex (regex, noFlags, replace, parseFlags, Regex)
+import Data.Either (fromRight)
+import Partial.Unsafe (unsafePartial)
+
 example1 :: Rendered
 example1 = render do
   color red
@@ -46,8 +55,50 @@ nestedNodesWithEmptyParent = render do
 assertEqual :: forall a. (Eq a, Show a) => a -> a -> Eff (err :: EXCEPTION) Unit
 assertEqual x y = unless (x == y) <<< throwException <<< error $ "Assertion failed: " <> show x <> " /= " <> show y
 
+
+
+
+data CssId = TheWrapper | TheContent
+derive instance genericCssId :: Generic CssId
+
+data CssClass = WrapperFTW | SomeContent
+derive instance genericCssClass :: Generic CssClass
+
+-- instance showCssClass :: Show CssClass where
+--   show = gShow
+
+
+toId :: CssId -> Predicate
+toId = Id <<< spineCase <<< predicateName
+
+toClass :: CssClass -> Predicate
+toClass = Class <<< spineCase <<< predicateName
+
+toClassSel :: CssClass -> Selector
+toClassSel c = Selector (Refinement [toClass c]) Star
+
+spineCase :: String -> String
+spineCase = drop 1 <<< toLower <<< replace matchCaps "-$&"
+
+predicateName :: forall a. (Generic a) => a -> String
+predicateName = fromMaybe "undefined" <<< last <<< split "." <<< gShow
+
+matchCaps :: Regex
+matchCaps = unsafePartial $ fromRight $ regex "[A-Z]" (parseFlags "g")
+
+
+example6 :: Rendered
+example6 = render do
+  toClassSel WrapperFTW ? do
+    display block
+
+
 main :: Eff (err :: EXCEPTION) Unit
 main = do
+
+  selector (Selector (Refinement [toClass WrapperFTW]) Star) `assertEqual` ".wrapper-f-t-w"
+  renderedSheet example6 `assertEqual` Just ".wrapper-f-t-w { display: block }\n"
+
   renderedInline example1 `assertEqual` Just "color: hsl(0.0, 100.0%, 50.0%); display: block"
   renderedInline example2 `assertEqual` Just "display: inline-block"
   renderedInline example3 `assertEqual` Just "border: dashed 2.0px hsl(240.0, 100.0%, 50.0%) "
